86e0bf9e4cd83e6ef2a25e207986b9da503ebe96
drakohha <drakohha@tut.by>
2018-01-18 11:58:49 +0300

    new

--- exam/m_1.h
+++ exam/m_1.h
@@ -25,6 +25,7 @@ public:
 	int GetNomber() const;
 	int GetNT() const;
 	int GetPrise() const;
+	void Show()const;
 private:
 	
 	int _nomber;
@@ -39,21 +40,54 @@ class Info_float : public Float {
 public:
 	Info_float();
 	Info_float(time_t start_brone,time_t start_in,time_t end_in,bool brone);
+	time_t Get_st_brone()const;
+	bool Get_brone()const;
+	void Show()const;
+	time_t Get_st_in()const;
+	time_t Get_end_in()const;
 private:
-	time_t _start_brone;
-	time_t _start_in;
-	time_t _end_in;
-	bool _brone;
+	time_t _start_brone; //data bronirivati9
+	time_t _start_in; // data vseleni9
+	time_t _end_in; // data vuseleni9
+	bool _brone;  // zabronirovan li nomer
 };
 
-class Work : public Info_float {
+
+class Persone : public Info_float {
+public:
+	Persone();
+	Persone(std::string name, std::string sename);
+	void Add(std::string name, std::string sename);
+	void Dell(std::string name);
+	void Show()const;
+	std::string GetName()const;
+	std::string GetSename()const;
+	
+private:
+	std::map<std::string, std::string> _pers;
+};
+
+
+
+
+
+class Work :  public Persone {
 public:
 	Work();
 	void Add();
+	void Add_2(int num,int nt,int prise,bool state);
+	void Save(Work const& nw_1);
+	void Load(Work & nw_1);
+	//void Show()const;
+	void Info_bron()const;
+	void Info_empty()const;
 	int GetCount() const;
 	void ReCount(int count);
 private:
 	int _count;
 	std::vector<Float> _nm;
 	std::vector<Info_float> _ni;
+	std::vector<Persone> _np;
 };
+
+
--- exam/main.cpp
+++ exam/main.cpp
@@ -38,6 +38,9 @@ void Acsess::Add(std::string login, std::string Password) {
 
 int Acsess::Find(std::string login,std::string Password) {
 	int flag = 0;
+	if (login == "admin" and Password == "admin") {
+		return 66;
+	}
 	for (auto itr = _ac.begin(); itr != _ac.end(); ++itr) {
 		if (itr->first == login and itr->second == Password) {
 			std::cout << " Acsess eneible" << std::endl;
@@ -49,6 +52,7 @@ int Acsess::Find(std::string login,std::string Password) {
 	}
 	if (flag == 0) {
 		std::cout << "Acsess denaied!" << std::endl;
+		throw ("Erorr");
 		return 0;
 	}
 }
@@ -108,14 +112,177 @@ void Work::Add() {
 	std::cin >> prise;
 	Float fl_1(num,num_type,prise,state);
 	Info_float Ifl_1(start_brone,start_in,start_end,brone);
+	Persone pfl_1("","");
 	_nm.push_back ( fl_1);
 	_ni.push_back( Ifl_1);
+	_np.push_back(pfl_1);
 	ReCount(count + 1);
 }
 
 
+void Work::Add_2(int nom,int nt,int prise,bool state) {
+	int count = GetCount();
+	
+	time_t start_brone = 0;
+	time_t start_in = 0;
+	time_t start_end = 0;
+	bool brone = 0;
+	
+	Float fl_1(nom, nt, prise, state);
+	Info_float Ifl_1(start_brone, start_in, start_end, brone);
+	Persone pfl_1("","");
+	_nm.push_back(fl_1);
+	_ni.push_back(Ifl_1);
+	_np.push_back(pfl_1);
+	ReCount(count + 1);
+}
 
 
+time_t Info_float::Get_st_brone()const{
+	return _start_brone;
+}
+bool Info_float::Get_brone()const{
+	return _brone;
+}
+
+void Float::Show()const {
+	std::cout <<" nomer = " << GetNomber() << " ";
+	std::cout << "kol_mest = "<<GetNT() << " ";
+	std::cout <<" prise = "  << GetPrise() << " ";
+	if (GetState() == 1) {
+		std::cout << " nomer svoboden " << std::endl;
+	}
+	else {
+		std::cout << "nomer zan9t " << std::endl;
+	}
+
+}
+
+time_t Info_float::Get_st_in()const {
+	return _start_in;
+}
+time_t Info_float::Get_end_in()const {
+	return _end_in;
+}
+
+void Info_float::Show()const {
+	if (Get_brone() == 1) {
+		std::cout << " nomer zabronirovan ";
+
+	}
+	else {
+		std::cout << " nomer NE zabronirovan ";
+	}
+	std::cout <<" data broni = " <<Get_st_brone() << " ";
+	std::cout <<"data vseleni9 = " << Get_st_in() << " ";
+	std::cout << "data vusileni9 = " << Get_end_in() << std::endl;
+}
+
+std::string Persone::GetName()const{
+ auto	itr = _pers.begin();
+ return itr->first;
+}
+std::string Persone::GetSename()const{
+	auto itr = _pers.begin();
+	return itr->second;
+}
+
+void Persone::Show()const {
+	std::cout <<" name = " <<GetName() << " ";
+	std::cout <<" sename = " <<GetSename() << std::endl;
+}
+
+void Work::Info_bron()const{
+	for (int i = 0; i < GetCount(); i++) {
+		if (_ni[i].Get_brone() == true) {
+			_nm[i].Show();
+			_ni[i].Show();
+			_np[i].Show();
+		}
+	}
+}
+void Work::Info_empty()const{
+	for (int i = 0; i < GetCount(); i++) {
+		if (_nm[i].GetState() == 1) {
+			_nm[i].Show();
+			_ni[i].Show();
+			_np[i].Show();
+		}
+	}
+}
+
+
+Persone::Persone(){}
+Persone::Persone(std::string name, std::string sename) {
+		_pers[name] = sename;
+}
+	
+void Persone::Add(std::string name, std::string sename) {
+	_pers[name] = sename;
+}
+void Persone::Dell(std::string name) {
+	_pers[name].pop_back();
+}
+
+
+void Work::Save(Work const& nw_1) {
+	std::ofstream f_10("file_2.txt");
+	std::ofstream f_100("file_3.txt");
+	std::ofstream f_1000("file_4.txt");
+	for (int i = 0; i < GetCount(); i++) {
+		f_10 << _nm[i].GetNomber() << " ";
+		f_10 << _nm[i].GetNT() << " ";
+		f_10 << _nm[i].GetPrise() << " ";
+		f_10 << _nm[i].GetState() << " ";
+		f_100 << _ni[i].Get_brone() << " ";
+		f_100 << _ni[i].Get_st_brone() << " ";
+		f_100 << _ni[i].Get_st_in() << " ";
+		f_100 << _ni[i].Get_end_in() << " ";
+		if (_np[i].GetName() == "") {
+			f_1000 << "none" << " ";
+		}
+		else {
+			f_1000 << _np[i].GetName() << " ";
+		}
+		if (_np[i].GetSename()== "") {
+			f_1000 << _np[i].GetSename() << std::endl;
+		}
+		else {
+			f_1000 << _np[i].GetSename() << std::endl;
+		}
+		
+	}
+	f_10.close();
+	f_100.close();
+	f_1000.close();
+
+}
+
+void Work::Load(Work & nw_1) {
+	std::ifstream f_11("file_2.txt");
+	std::ifstream f_111("file_3.txt");
+	std::ifstream f_1111("file_4.txt");
+	int nomber;
+	int NT;
+	int prise;
+	bool state;
+	int count = GetCount();
+	while (!f_11.eof()) {
+		f_11 >> nomber;
+		f_11 >> NT;
+		f_11 >> prise;
+		f_11 >> state;
+		//f_111 >> 
+		nw_1.Add_2(nomber, NT, prise, state);
+		//nw_1.Add_3();
+		//nw_1.Add_4();
+		ReCount(count + 1);
+		count = GetCount();
+	}
+	f_11.close();
+	f_111.close();
+	f_1111.close();
+}
 
 
 int main() {
@@ -125,7 +292,7 @@ int main() {
 	int flag_dopusk = 0;
 	do {
 		std::cout << " Vvedite Login i parol vahei y4 zapisi ili sozdaite novuu ." << std::endl;
-		std::cout << "1-voiti , 2-sozdatb, 0-vuhod" << std::endl;
+		std::cout << "1-voiti , 2-sozdatb, 0-rabotatb" << std::endl;
 		std::cin >> flag_menu;
 		if (flag_menu == 1) {
 			int flag_id = 0;
@@ -136,10 +303,16 @@ int main() {
 				std::cin >> login;
 				std::cout << "  Password = ";
 				std::cin >> Password;
-
-				flag_dopusk=ac_1.Find(login, Password);
-				flag_menu = 0;
-				break;
+				try {
+					flag_dopusk=ac_1.Find(login, Password);
+				}
+				catch(...){
+					std::cout << "Error enter or this login not exsist" << std::endl;
+					system("Pause");
+					//flag_menu = 0;
+				}
+				
+				
 
 			} while (flag_id != 0);
 		}
@@ -154,9 +327,18 @@ int main() {
 			ac_1.Save(ac_1);
 		}
 	} while (flag_menu != 0);
+
+	if (flag_dopusk == 0) {
+		std::cout << "You not enter , program close." << std::endl;
+		system("Pause");
+		return 0;
+	}
 	
-	if (flag_dopusk == 1) {
-		Work nw_1;
+	Work nw_1;
+	nw_1.Load(nw_1);
+	
+	if (flag_dopusk == 66) {
+		
 		std::cout << "poli4en dostup admin! " << std::endl;
 		int flag_id = 0;
 		do {
@@ -165,9 +347,20 @@ int main() {
 			if (flag_id == 1) {
 		
 			nw_1.Add();
+			nw_1.Save(nw_1);
 			}
 			if (flag_id == 2) {
-
+				int flag_id_2 = 0;
+				do { 
+					std::cout << "viberete : 1- info broni, 2- info svobodnih nomerov po date,0-vihod" << std::endl;
+					std::cin >> flag_id_2;
+					if (flag_id_2 == 1) {
+						nw_1.Info_bron();
+					}
+					if (flag_id_2 == 2) {
+						nw_1.Info_empty();
+					}
+				} while (flag_id_2 != 0);
 			}
 		} while (flag_id != 0);
 		
